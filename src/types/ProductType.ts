43a813082580876649a5d23e8ac947aa33bfde75
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-11-12 00:23:30 +0500

    make components

---
+++ src/types/ProductType.ts
@@ -0,0 +1,9 @@
+export type ProductType = {
+    images:string;
+    title:string;
+    description:string;
+    price:number;
+    discountPercentage:number;
+    rating:number;
+    finalPrice:number;
+}
